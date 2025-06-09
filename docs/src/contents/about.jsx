8a91b2141ca04af6b51a0623991f082fc3164c92
ForceNight <89636568+elmesiashu@users.noreply.github.com>
2025-06-09 00:28:10 -0700

    owo

--- docs/src/contents/about.jsx
+++ docs/src/contents/about.jsx
@@ -7,16 +7,14 @@ export default function about() {
             <h1>About me</h1>
             <div className="about-container">
                 {/* sentence information */}
-                <div className="about-child">
-                    <div className="about-info">          
-                        <div className="about-text">
+                <div className="about-child">      
+                    <div className="about-text">
                         <p>A passionate IT student at Kwantlen Polytechnic University, driven by a strong desire to grow and expand skills in software 
                             development and coding. Eager to explore and master new technologies through both academic learning and practical application. 
                             Actively engaged in group projects that involve building full-stack solutions, including websites, web applications, and software 
                             tools with experience across frontend design and backend development. Enthusiastic about hands-on exercises, real-world challenges, 
                             and collaborative environments that foster continuous learning and personal growth. Adaptable, self-motivated, and committed to 
                             lifelong learning in the ever-evolving tech landscape.</p>
-                        </div>
                     </div>
                 </div>
 
@@ -27,33 +25,33 @@ export default function about() {
                         <ul>
                             <li>
                                 <a href="">
-                                    <button className="btn1" disabled>Melody Ann Fuentes</button>
+                                    <button className="btn1" disabled><p>Melody Ann Fuentes</p></button>
                                 </a>
                             </li>
                             <li>
                                 <a href="">
-                                    <button className="btn1" disabled >BTECH - Information Technology</button>
+                                    <button className="btn1" disabled ><p>BTECH - Information Technology</p></button>
                                 </a>
                             </li>
                             <li>
                                  <a href="https://www.kpu.ca/">
-                                    <button className="btn">Kwantlen Polytechnic University</button>
+                                    <button className="btn"><p>Kwantlen Polytechnic University</p></button>
                                 </a>
                             </li>
                         </ul>
                         <ul>
                             <li>
                                 <a href="https://www.linkedin.com/in/melodyannfuentes/">
-                                    <button className="btn">LinkedIn</button>
+                                    <button className="btn"><p>LinkedIn</p></button>
                                 </a>
                             </li>
                             <li>
                                 <a href="https://github.com/elmesiashu">
-                                    <button className="btn">GitHub</button>
+                                    <button className="btn"><p>GitHub</p></button>
                                 </a>
                             </li>
                             <li><a href="MAF_Resume.pdf" download>
-                                    <button className="btn">Download Resume/CVV</button>
+                                    <button className="btn"><p>Download Resume/CVV</p></button>
                                 </a>
                             </li>
                         </ul>
